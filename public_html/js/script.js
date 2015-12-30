0cfa467c30104ada64d286094628a96923894755
Ming wei Chan <mcha1524@uni.sydney.edu.au>
2015-12-30 13:57:45 +1100

    2

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -1,28 +1,81 @@
 $(document).ready(function(){
-    $(".work ul li a").on("mouseenter", function() {
-        $(this).animate({
-            left: "-=40px",
+    
+//HIGHLIGHT MOVE TEXT LI
+    
+    $(".work ul li").on("mouseenter", function() {
+        $(this).finish().animate({
+            marginLeft: "-40px",
         }, 200)
     });
-    $(".work ul li a").on("mouseleave", function() {
-        $(this).animate({
-            left: "+=40px",
+    $(".work ul li").on("mouseleave", function() {
+        $(this).finish().animate({
+            marginLeft: "0px",
         }, 200)
     });
+    
+//THUMBNAIL FADE IN
+    
     $("#darl").on({ 
         mouseenter: function() {
-            $("#bgimg1").fadeIn(200);
+            $("#bgimg1").stop(true,true).fadeIn(200);
     },
         mouseleave: function() {
-            $("#bgimg1").fadeOut(200);
+            $("#bgimg1").stop(true,true).fadeOut(200);
         }
     });
     $("#chil").on({ 
         mouseenter: function() {
-            $("#bgimg2").fadeIn(200);
+            $("#bgimg2").stop(true,true).fadeIn(200);
     },
         mouseleave: function() {
-            $("#bgimg2").fadeOut(200);
+            $("#bgimg2").stop(true,true).fadeOut(200);
         }
     });
-})
+    $("#lawn").on({ 
+        mouseenter: function() {
+            $("#bgimg3").stop(true,true).fadeIn(200);
+    },
+        mouseleave: function() {
+            $("#bgimg3").stop(true,true).fadeOut(200);
+        }
+    });
+    $("#aque").on({ 
+        mouseenter: function() {
+            $("#bgimg4").stop(true,true).fadeIn(200);
+    },
+        mouseleave: function() {
+            $("#bgimg4").stop(true,true).fadeOut(200);
+        }
+    });
+    $("#velo").on({ 
+        mouseenter: function() {
+            $("#bgimg5").stop(true,true).fadeIn(200);
+    },
+        mouseleave: function() {
+            $("#bgimg5").stop(true,true).fadeOut(200);
+        }
+    });
+    //EXPAND AND CLOSE DIV
+   
+    var printF = function(){console.log($("#darl").height())};
+    
+    $("#darl").on("click", function(){
+        if($("#darl").height() == 38){
+        
+            $(this).finish().animate({
+                height: "300px",
+            }, 500);
+            $("#darl .block").css("overflow", "visible");
+            $(this).off('mouseenter');
+        }else{
+            $(this).finish().animate({
+               height: "38px",
+            }, 500);  
+        }
+    printF();
+            });
+   
+        
+    
+});
+    
